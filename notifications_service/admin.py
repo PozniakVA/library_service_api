688995fb9411536917470d6f1e153e1b272f0929
Valentin Pozniak <waliker448@gmail.com>
2024-10-12 19:12:05 +0300

    add model Chat and register in admin.py

--- notifications_service/admin.py
+++ notifications_service/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from notifications_service.models import Chat
+
+
+@admin.register(Chat)
+class BorrowingsAdmin(admin.ModelAdmin):
+    pass
--- notifications_service/models.py
+++ notifications_service/models.py
@@ -1,3 +1,14 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class Chat(models.Model):
+    user = models.ForeignKey(
+        get_user_model(),
+        on_delete=models.CASCADE,
+        related_name="chats",
+    )
+    chat_id = models.IntegerField(null=True)
+
+    def __str__(self) -> str:
+        return self.user.first_name
