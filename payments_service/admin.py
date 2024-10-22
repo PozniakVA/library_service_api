1ab6eebf91438f77c14a9b12aeed3492c22c9e03
Valentin Pozniak <waliker448@gmail.com>
2024-10-22 20:06:30 +0300

    register Payments model in admin

--- payments_service/admin.py
+++ payments_service/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from payments_service.models import Payments
+
+
+@admin.register(Payments)
+class BorrowingsAdmin(admin.ModelAdmin):
+    pass
