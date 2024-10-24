5df0a801ca516311cdead1ed0ef1649eaa52a8fb
Valentin Pozniak <waliker448@gmail.com>
2024-10-24 20:18:57 +0300

    add validation on expected_return_date field

--- borrowings_service/models.py
+++ borrowings_service/models.py
@@ -1,12 +1,14 @@
-from django.contrib.auth import get_user_model
 from django.db import models
+from django.utils import timezone
+from rest_framework.exceptions import ValidationError
 
 from books_service.models import Book
+from users_service.models import User
 
 
 class Borrowings(models.Model):
     borrow_date = models.DateTimeField(auto_now_add=True, editable=False)
-    expected_return_date = models.DateTimeField() # TODO: add the restriction that the return date cannot beat in the past
+    expected_return_date = models.DateTimeField()
     actual_return_date = models.DateTimeField(null=True, editable=False)
     book = models.ForeignKey(
         Book,
@@ -14,10 +16,18 @@ class Borrowings(models.Model):
         on_delete=models.CASCADE
     )
     user = models.ForeignKey(
-        get_user_model(),
+        User,
         related_name="borrowings",
         on_delete=models.CASCADE
     )
 
+    def clean(self):
+        if self.expected_return_date < timezone.now():
+            raise ValidationError("Expected return date cannot be in the past")
+
+    def save(self, *args, **kwargs):
+        self.clean()
+        super().save(*args, **kwargs)
+
     def __str__(self) -> str:
-        return f"{self.borrow_date} - {self.expected_return_date}"
+        return f"{self.user.email} - {self.book.title} - {self.expected_return_date}"
